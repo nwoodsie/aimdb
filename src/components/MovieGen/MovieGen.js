fe8885c781b420f72c11d3fbba65198a5ce1bffb
Saloni Aphale <81273312+saloniaphale@users.noreply.github.com>
2022-11-06 19:31:16 +1100

    made movie gen directory

--- src/components/MovieGen/MovieGen.js
+++ src/components/MovieGen/MovieGen.js
@@ -2,15 +2,30 @@ import React from 'react'
 import './MovieGen.css'
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import {Link} from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import ReactDOM from 'react-dom';
 
 function MovieGen() {
   return (
-    <div className="infoBlockWrapper2">
-      <div className="textWrapper2">
-        <b>Generated Movies</b>
-        View list of all generated movies
-      </div>
-    </div>
+   <form>
+     <div >
+       <label>
+         <a>
+             Name: </a>
+          <input type="text"  />
+      </label>
+     </div>
+       <div>
+            <label >
+        Saloni:
+          <input type="text" name ="name"/>
+      </label>
+       </div>
+       <div>
+                   <button type = "submit"> Submit</button>
+       </div>
+  {/*<input type="submit" value="enter" />*/}
+</form>
   );
 }
 
